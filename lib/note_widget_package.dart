8baabf7d57b50d88853ef1475630ddf8179eae79
DanielWillforss <willforss66@gmail.com>
2026-01-27 12:32:56 +0100

    first commit

---
+++ lib/note_widget_package.dart
@@ -0,0 +1,3 @@
+export 'src/note_detail_page.dart';
+export 'src/notes_api.dart';
+export 'src/notes_page.dart';
---
+++ lib/src/notes_api.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:notes_repo_core/note_package.dart';
+
+class NotesApi {
+  static const String baseUrl = 'http://127.0.0.1:5000/notes';
+
+  /// GET /notes
+  static Future<List<Note>> getNotes() async {
+    final response = await http.get(Uri.parse('$baseUrl'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load notes');
+    }
+
+    final List data = jsonDecode(response.body);
+    return data.map((e) => Note.fromMap(e)).toList();
+  }
+
+  /// POST /notes
+  static Future<Note> createNote(String title, String body) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'title': title, 'body': body}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to create note');
+    }
+
+    final Note note = Note.fromMap(jsonDecode(response.body));
+    return note;
+  }
+
+  /// PUT /notes/{id}
+  static Future<Note> updateNote(int id, String title, String body) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'title': title, 'body': body}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update note');
+    }
+
+    final Note note = Note.fromMap(jsonDecode(response.body));
+    return note;
+  }
+
+  /// DELETE /notes/{id}
+  static Future<void> deleteNote(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete note');
+    }
+  }
+}
